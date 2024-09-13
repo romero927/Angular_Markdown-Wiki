efefab18b7a06e5b2ee78a09b8acef3010926b5f
Kyle Romero <kgromero@gmail.com>
2024-09-13 18:24:41 -0400

    Update to auto generate navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('sidenav') sidenav: MatSidenav;
+  
+  // Array to hold the pages from pages.json
+  pages: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  // This will run when the component is initialized
+  ngOnInit() {
+    // Fetch the pages.json file and populate the pages array
+    this.http.get<any[]>('assets/pages.json').subscribe(data => {
+      this.pages = data;
+    });
+  }
 
   toggleSidenav() {
     this.sidenav.toggle();
